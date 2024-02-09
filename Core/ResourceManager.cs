7b27095598a7f752e6f1aa7f4c4f5bb9e7abc896
KSilhouette <prestongregory125@gmail.com>
2024-02-09 10:50:39 -0700

    Update

---
+++ Core/ResourceManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.Json;
+
+namespace KheaiGameEngine.Core
+{
+    internal class ResourceManager
+    {
+        void Load()
+        {
+            Type type = Type.GetType("");
+
+            if (type != null) 
+            {
+
+            }
+
+            JsonSerializer.Deserialize("", type);
+        }
+    }
+}
